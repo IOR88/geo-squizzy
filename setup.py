e37dc27318094dc6c771ee21a2d927aa98913745
ior88 <t-32@o2.pl>
2016-02-14 21:57:52 +0100

    init-package

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='geo-squizzy',
+    version='0.1',
+    packages=find_packages(exclude=['tests*']),
+    license='MIT',
+    description='GeoJSON-unknown-documents-model-creation',
+    long_description=open('README.md').read(),
+    # install_requires=['numpy'],
+    url='https://github.com/LowerSilesians/geo-squizzy',
+    author='Igor Miazek, Jakub Miazek',
+    author_email='t-32@o2.pl, the@grillazz.com'
+)
