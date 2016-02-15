12382fa67f5d7b2216823605621198a84713e0c5
ior88 <t-32@o2.pl>
2016-02-15 22:04:09 +0100

    data

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='geo-squizzy',
     version='0.1',
-    packages=find_packages(exclude=['tests*']),
+    packages=find_packages(exclude=['tests*', 'research*', 'build_big_data*']),
     license='MIT',
     description='GeoJSON-unknown-documents-model-creation',
     long_description=open('README.md').read(),
