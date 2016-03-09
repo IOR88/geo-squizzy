a56469d9786b55ca50f2eadd1e2b6635279be751
grillazz <the@grillazz.com>
2016-03-09 10:22:35 +0100

    make imports bulletproof for python2&3

--- tests/getdata.py
+++ tests/getdata.py
@@ -1,11 +1,16 @@
-from urllib import request
+try:
+    from urllib.request import urlopen
+    from urllib.parse import urlparse
+except ImportError:
+    from urlparse import urlparse
+    from urllib import urlopen
 
 
 def get_geojson(url=None):
     """
     get url data and decode it
     """
-    res = request.urlopen(url=url)
+    res = urlopen(url=url)
     with res as f:
         while True:
             return f.read().decode('utf-8')
